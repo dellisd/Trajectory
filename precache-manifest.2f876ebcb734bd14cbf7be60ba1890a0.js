09af5c2b50659fe8cbd87dbfba3f85850baeba11
Omar <omar.garciaf247@gmail.com>
2020-05-19 16:02:32 -0400

    Updates

---
+++ precache-manifest.2f876ebcb734bd14cbf7be60ba1890a0.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2f1f2cc9319b516d4fd7afdf74a274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3613c4b3710b0034e3ee",
+    "url": "/static/css/main.2c000932.chunk.css"
+  },
+  {
+    "revision": "bf06ad071c77e936d4f6",
+    "url": "/static/js/2.9fd8d504.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/static/js/2.9fd8d504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3613c4b3710b0034e3ee",
+    "url": "/static/js/main.06ff087e.chunk.js"
+  },
+  {
+    "revision": "f1b8cb8ba2ed8ff3eb19",
+    "url": "/static/js/runtime-main.58c4d41b.js"
+  },
+  {
+    "revision": "08ed1c65b67a55a10fad9a960018eb37",
+    "url": "/static/media/bus-light.08ed1c65.svg"
+  },
+  {
+    "revision": "aaebb65cdf648ae0ec1aab1f42d8c51a",
+    "url": "/static/media/button-down.aaebb65c.svg"
+  },
+  {
+    "revision": "0b0ece0aed0430173ba800c1da352f06",
+    "url": "/static/media/button-left.0b0ece0a.svg"
+  },
+  {
+    "revision": "c7f6c1d17f49976a85c43b07dfd2423d",
+    "url": "/static/media/button-right.c7f6c1d1.svg"
+  },
+  {
+    "revision": "a319f4adf377bac609642d44c5e162fd",
+    "url": "/static/media/gotrain-light.a319f4ad.svg"
+  },
+  {
+    "revision": "764ad82caf3b0ba499f33d4208dc9c54",
+    "url": "/static/media/location-animated.764ad82c.svg"
+  },
+  {
+    "revision": "602d69c737dedc20fe3eed788b99958d",
+    "url": "/static/media/location.602d69c7.svg"
+  },
+  {
+    "revision": "1ca208935b41ec4250b5025681d0ff81",
+    "url": "/static/media/road-animated.1ca20893.svg"
+  },
+  {
+    "revision": "d04f9a087c8ff50fe2c17f33f747a98a",
+    "url": "/static/media/road.d04f9a08.svg"
+  },
+  {
+    "revision": "ae9f9b5c7a2a61bf3a53f0db3e4d7b8b",
+    "url": "/static/media/search-light.ae9f9b5c.svg"
+  },
+  {
+    "revision": "49c5cc9de957009367b44be19d19cfea",
+    "url": "/static/media/streetcar-light.49c5cc9d.svg"
+  },
+  {
+    "revision": "447839b397be97edbce79eb1b3d040e2",
+    "url": "/static/media/subway-light.447839b3.svg"
+  },
+  {
+    "revision": "b079ed2a488e15afc08ad80977434db8",
+    "url": "/static/media/trajectory_logo.b079ed2a.svg"
+  },
+  {
+    "revision": "c8a8aee21a107377fb94a83888733ad2",
+    "url": "/static/media/via-light.c8a8aee2.svg"
+  },
+  {
+    "revision": "0682f0e4846a607de09d7778b5ebfd3c",
+    "url": "/static/media/visible-animated.0682f0e4.svg"
+  },
+  {
+    "revision": "673c66305e4cc0eb6a1f54156140f1eb",
+    "url": "/static/media/visible.673c6630.svg"
+  },
+  {
+    "revision": "37b6c3d7cdcb9d6948984593aa9b8b80",
+    "url": "/static/media/wheel.37b6c3d7.svg"
+  }
+]);
